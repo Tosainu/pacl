aaf045313ba9829fd304f4ff5c125aa77627b275
Kenta Sato <tosainu.maple@gmail.com>
2021-04-04 22:04:11 +0900

    parse_command_line

---
+++ src/cli.rs
@@ -0,0 +1,64 @@
+use crate::error::{Error, ErrorKind, Result};
+use crate::url::normalize_repo_url;
+
+#[derive(Debug)]
+pub enum Args {
+    Help, // -h, --help
+    Clone {
+        base_dir: Option<String>, // -b, --base-dir
+        url: url::Url,            // <url>
+        extra_args: Vec<String>,  // -- [extra git args] ...
+    },
+}
+
+pub fn print_usage(exe_name: &str) {
+pub fn print_usage() {
+    println!("usage:");
+    println!("    pacl [options]... <repository url> [-- [extra args passed to git]...]");
+    println!();
+    println!("options:");
+    println!("    -h, --help            display this messages and exit");
+    println!("    -b, --base-dir <dir>  base directory to clone");
+}
+
+pub fn parse_command_line() -> Result<Args> {
+    let mut args = std::env::args();
+    args.next();
+
+    let mut base_dir = None;
+    let mut url = None;
+    let mut extra_args = None;
+
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "-h" | "--help" => return Ok(Args::Help),
+
+            "-b" | "--base-dir" => base_dir = Some(args.next().unwrap()),
+
+            "--" => {
+                extra_args = Some(args.collect());
+                break;
+            }
+
+            _ => {
+                if url.is_none() {
+                    url = Some(arg)
+                } else {
+                    return Err(Error::new(ErrorKind::InvalidArg(Some(arg))));
+                }
+            }
+        }
+    }
+
+    if let Some(url) = url {
+        Ok(Args::Clone {
+            base_dir,
+            url: normalize_repo_url(url)?,
+            extra_args: extra_args.unwrap_or_default(),
+        })
+    } else {
+        Err(Error::new(ErrorKind::MissingRequiredArg(
+            "<url>".to_owned(),
+        )))
+    }
+}
--- src/error.rs
+++ src/error.rs
@@ -16,6 +16,8 @@ pub type Result<T> = std::result::Result<T, Box<Error>>;
 #[derive(Debug)]
 pub enum ErrorKind {
     InvalidUrl(url::ParseError),
+    InvalidArg(Option<String>),
+    MissingRequiredArg(String),
 }
 
 impl fmt::Display for Error {
@@ -28,6 +30,14 @@ impl fmt::Display for ErrorKind {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             ErrorKind::InvalidUrl(e) => e.fmt(f),
+            ErrorKind::InvalidArg(arg) => {
+                write!(f, "unknown / invalid commandline argument")?;
+                if let Some(arg) = arg {
+                    write!(f, " '{}'", arg)?;
+                }
+                Ok(())
+            }
+            ErrorKind::MissingRequiredArg(arg) => write!(f, "missing required argument '{}'", arg),
         }
     }
 }
